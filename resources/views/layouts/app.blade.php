c96c827cd29626d8e69ef65cc441383f48d13bb5
Hussein Najah <husseinnajah123@gmail.com>
2023-01-11 22:26:18 +0300

    install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,19 +28,21 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@500&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.4/dist/flowbite.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}">
     @livewireStyles
     <!-- Scripts -->
     <script src="{{ mix('resources/js/app.js') }}" defer></script>
 </head>
 
-<body id="home" class=" h-screen home ">
+<body id="home" class="h-screen  home">
     <!-- <x-jet-banner /> -->
     <div class="min-h-screen ">
-        <div class=" grid">
+        <div class="grid ">
             @if (Route::currentRouteName() != 'home')
-            @livewire('components.sidebar')
+                @livewire('components.sidebar')
             @endif
             <!-- Page Content -->
             <main class="@if (Route::currentRouteName() != 'home') mt-4 mr-96 @endif">
@@ -55,7 +57,9 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('vendor/livewire-alert/livewire-alert.js') }}"></script>
     <script src="https://unpkg.com/flowbite@1.4.4/dist/flowbite.js" defer></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
+        integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </body>
 

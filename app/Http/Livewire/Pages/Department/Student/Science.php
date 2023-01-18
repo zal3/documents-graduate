b1cbf9d1348f8c1e26546b60878f1e0597443216
zal3 <102377778+zal3@users.noreply.github.com>
2023-01-18 21:37:13 +0300

    ش

--- app/Http/Livewire/Pages/Department/Student/Science.php
+++ app/Http/Livewire/Pages/Department/Student/Science.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Livewire\Pages\Department\Student;
-
+use App\Models\Student;
 use Livewire\Component;
 
 class Science extends Component
 {
     public function render()
     {
-        return view('livewire.pages.department.student.science');
+        $students = Student::with('department')->where('department_id' , 1)->get();
+        return view('livewire.pages.department.student.science', compact('students'));
     }
 }

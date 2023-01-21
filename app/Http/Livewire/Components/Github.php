85650a5d54682bb74d61ffcf6e751fa850462922
Hussein Najah <husseinnajah123@gmail.com>
2023-01-21 16:46:01 +0300

    Github logo

---
+++ app/Http/Livewire/Components/Github.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Livewire\Component;
+
+class Github extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.github');
+    }
+}

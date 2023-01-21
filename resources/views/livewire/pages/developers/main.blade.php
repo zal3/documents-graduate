e5a38b866da8fc806c4e1f3ade3a480c2de27fa3
Hussein Najah <husseinnajah123@gmail.com>
2023-01-21 16:58:55 +0300

    data filling

--- resources/views/livewire/pages/developers/main.blade.php
+++ resources/views/livewire/pages/developers/main.blade.php
@@ -1,45 +1,47 @@
 <div>
-    <div class="p-4 m-[12%]">
-        <h1 class="text-3xl font-medium">About Us</h1>
-        <p class="text-lg">We are a team of dedicated professionals working towards a common goal of providing top-notch
-            services to our clients. With years of experience in our respective fields, we strive to deliver the best
-            results every time.</p>
+    <div class="p-4 m-[12%]" dir="rtl">
+        <h1 class="text-3xl font-medium">من نحن</h1>
+        <p class="text-lg">نحن فريق من المهنيين المتفانين الذين يعملون من أجل تحقيق هدف مشترك يتمثل في تقديم خدمات من
+            الدرجة الأولى لعملائنا. مع سنوات من الخبرة في مجالات تخصصنا ، نسعى جاهدين لتقديم أفضل النتائج في كل مرة.</p>
         <div class="my-4">
-            <h2 class="text-xl font-medium">Our Team</h2>
-            <div class="flex flex-wrap -mx-2">
+            <h2 class="text-xl font-medium">فريقنا</h2>
+            <div class="flex flex-wrap -mx-2" dir="ltr">
                 <div class="w-1/3 px-2 mb-4">
-                    <div class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-blue-300">
+                    <div
+                        class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-blue-300">
                         <img class="w-16 mx-auto mb-4 bg-cover rounded-full" src="/img/developers/fatima.jpg"
                             alt="team member">
                         <h3 class="text-lg font-medium">Fatima Nawfal</h3>
                         <p class="text-gray-600">Web Developer</p>
-                        <a href="https://github.com/fatmanawfal" class="inline-block mx-2 duration-150 hover:text-blue-500" target="_blank"
-                            rel="noopener">
+                        <a href="https://github.com/fatmanawfal"
+                            class="inline-block mx-2 duration-150 hover:text-blue-500" target="_blank" rel="noopener">
                             @livewire('components.github')
                         </a>
                     </div>
                 </div>
                 <div class="w-1/3 px-2 mb-4">
-                    <div class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-blue-300">
-                        <img class="w-16 h-16 mx-auto mb-4 rounded-full"
-                            src="/img/developers/dhuha.jpg" alt="team member">
+                    <div
+                        class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-blue-300">
+                        <img class="w-16 h-16 mx-auto mb-4 rounded-full" src="/img/developers/dhuha.jpg"
+                            alt="team member">
                         <h3 class="text-lg font-medium">Dhuha Hamid</h3>
                         <p class="text-gray-600">Web Developer</p>
-                        <a href="https://github.com/dhuhahamid" class="inline-block mx-2 duration-150 hover:text-blue-500" target="_blank"
-                            rel="noopener">
+                        <a href="https://github.com/dhuhahamid"
+                            class="inline-block mx-2 duration-150 hover:text-blue-500" target="_blank" rel="noopener">
                             @livewire('components.github')
                         </a>
                     </div>
                 </div>
                 <div class="w-1/3 px-2 mb-4">
-                    <div class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-blue-300">
-                        <img class="w-16 h-16 mx-auto mb-4 rounded-full"
-                            src="/img/developers/naba.jpg" alt="team member">
+                    <div
+                        class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-blue-300">
+                        <img class="w-16 h-16 mx-auto mb-4 rounded-full" src="/img/developers/naba.jpg"
+                            alt="team member">
                         <h3 class="text-lg font-medium">Naba </h3>
                         <p class="text-gray-600">Web Developer</p>
                         {{-- TODO Add Naba Github like + father's name --}}
-                        <a href="https://github.com/fatmanawfal" class="inline-block mx-2 duration-150 hover:text-blue-500" target="_blank"
-                            rel="noopener">
+                        <a href="https://github.com/fatmanawfal"
+                            class="inline-block mx-2 duration-150 hover:text-blue-500" target="_blank" rel="noopener">
                             @livewire('components.github')
                         </a>
                     </div>
@@ -47,9 +49,9 @@
             </div>
         </div>
         <div class="my-4">
-            <h2 class="text-xl font-medium">Our Mission</h2>
-            <p class="text-lg">Our mission is to provide our clients with the highest quality services and products,
-                while maintaining an unwavering commitment to honesty and integrity.</p>
+            <h2 class="text-xl font-medium">مهمتنا</h2>
+            <p class="text-lg">مهمتنا هي تزويد عملائنا بأعلى جودة من الخدمات والمنتجات ، مع الحفاظ على التزام لا يتزعزع
+                بالأمانة والنزاهة.</p>
         </div>
     </div>
 

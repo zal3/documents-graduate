23a43f7efe7ece1881894a0a3d8b165f442f1d39
Hussein Najah <husseinnajah123@gmail.com>
2023-01-21 18:50:23 +0300

    Update main.blade.php

--- resources/views/livewire/pages/document/main.blade.php
+++ resources/views/livewire/pages/document/main.blade.php
@@ -1,9 +1,9 @@
 <div class="">
     <div class="">
-        <div class="bg-white mt-9  ml-44 border-4 border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
+        <div class="bg-white mt-9  ml-44 border-4 cursor-pointer border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
             <div class="p-5 mr-24 " dir="rtl">
                 <a">
-                    <img class="rounded-t-lg  -mt-4 -mr-28" dir="rtl" src="{{ asset('img/page.jpg') }}" alt="home photo" />
+                    <img class="-mt-4 rounded-t-lg -mr-28" dir="rtl" src="{{ asset('img/page.jpg') }}" alt="home photo" />
                     </a>
                     <a">
                         <h5 class="mb-2 -mt-20 text-2xl font-bold tracking-tight text-gray-900">وثيقة تخرج</h5>
@@ -11,11 +11,11 @@
                         <p class="mb-3 font-normal text-gray-700 ">باللغة العربية</p>
             </div>
         </div>
-        <div class="bg-white mt-9  ml-44 border-4 border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
+        <div class="bg-white mt-9  ml-44 border-4 cursor-pointer border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
 
             <div class="p-5 mr-24 " dir="rtl">
                 <a>
-                    <img class="rounded-t-lg  -mt-4 -mr-28" src="{{ asset('img/page.jpg') }}" alt="" />
+                    <img class="-mt-4 rounded-t-lg -mr-28" src="{{ asset('img/page.jpg') }}" alt="" />
                 </a>
                 <a>
                     <h5 class="mb-2 -mt-20 text-2xl font-bold tracking-tight text-gray-900">وثيقة تخرج</h5>
@@ -24,25 +24,25 @@
 
             </div>
         </div>
-        <div class=" bg-white mt-9  ml-44 border-4 border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
+        <div class=" bg-white mt-9  ml-44 border-4 cursor-pointer border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
             <div class="p-5 mr-24" dir="rtl">
                 <a">
-                    <img class="rounded-t-lg  -mt-4 -mr-28" src="{{ asset('img/page.jpg') }}" alt="" />
+                    <img class="-mt-4 rounded-t-lg -mr-28" src="{{ asset('img/page.jpg') }}" alt="" />
                     </a>
                     <a">
-                        <h5 class="mb-2  -mt-20 text-2xl font-bold tracking-tight text-gray-900">تأييد تخرج</h5>
+                        <h5 class="mb-2 -mt-20 text-2xl font-bold tracking-tight text-gray-900">تأييد تخرج</h5>
                         </a>
                         <p class="mb-3 font-normal text-gray-700 ">لغرض الدراسات</p>
             </div>
         </div>
-        <div class=" bg-white mt-9  ml-44 border-4 border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
+        <div class=" bg-white mt-9  ml-44 border-4 cursor-pointer border-[#FFCA28]  hover:border-[#071F63]  w-3/4 max-h-28 rounded-lg hover:transform transition duration-500 ease-in-out transform  hover:scale-105">
 
             <div class="p-5 mr-24" dir="rtl">
                 <a href="#">
-                    <img class="rounded-t-lg  -mt-4 -mr-28" src="{{ asset('img/page.jpg') }}" alt="" />
+                    <img class="-mt-4 rounded-t-lg -mr-28" src="{{ asset('img/page.jpg') }}" alt="" />
                 </a>
                 <a href="#">
-                    <h5 class="mb-2  -mt-20 text-2xl font-bold tracking-tight text-gray-900">تأييد تخرج</h5>
+                    <h5 class="mb-2 -mt-20 text-2xl font-bold tracking-tight text-gray-900">تأييد تخرج</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 ">قبل و بعد الامر الجامعي</p>
             </div>

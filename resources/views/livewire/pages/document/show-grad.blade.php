0f41e0945d72b94d1d05985d9511bb9935ed3454
Hussein Najah <husseinnajah123@gmail.com>
2023-01-26 02:12:46 +0300

    3th one

--- resources/views/livewire/pages/document/show-grad.blade.php
+++ resources/views/livewire/pages/document/show-grad.blade.php
@@ -1,92 +1,204 @@
-<div class="container flex flex-col justify-between  mx-auto px-4">
-    <div class="grid grid-cols-3 items-end ">
-        <div> </div>
-        <div class="text-center items-center">
-            <h2 class="text-center text-xl  font-semibold">تأييد تخرج </h2>
-            <h2 class="text-center text-xl  font-semibold">الى/من يمهمه الامر</h2>
-            <!-- @if($selected)
-                رقم التسلسل : <span class=" text-center font-semibold"> {{$student->id}}</span>
-                @endif -->
+<page size="A4" class="relative flex flex-col">
+    {{-- * Head Compartment --}}
+    <div class="border-2 border-black border-dashed">
+        <div class="flex justify-between p-5 pb-0 ">
+            <img class="h-[100px]" src="/img/un.png" alt="UOB">
+            <div>
+                <div class="flex flex-col items-center justify-center font-bold text-[14.5px] -mt-2">
+                    <p>جمهورية العراق</p>
+                    <p>Republic of Iraq</p>
+                    <p>وزارة التعليم و البحث العلمي</p>
+                    <p>Ministry of Higher Education & Scientific Reserach</p>
+                    <div class="flex gap-[100px]">
+                        <p>جامعة البصرة</p>
+                        <p>University of Basrah</p>
+                    </div>
+                    <div class="flex items-end gap-[20px]">
+                        <p>كلية علوم الحاسوب</p>
+                        <p>College of Computer Science</p>
+                    </div>
+                    <div class="flex gap-[30px]">
+                        <p>وتكنلوجيا المعلومات</p>
+                        <p>&Information Technology</p>
+                    </div>
+                    <div class="flex gap-[100px]">
+                        <p>وحدة الوثائق</p>
+                        <p>Documentation Office</p>
+                    </div>
+                </div>
+            </div>
+            <img class="h-[100px]" src="/img/it.png" alt="CSIT">
         </div>
-        @if($student->gender == 1)
-        <img src="{{ asset($student->image_path ?? 'img/female.png') }}" alt="" class="w-36 h-36 border-2  rounded-sm border-gray-900 p-1 mx-auto mt-10">
-        @else
-        <img src="{{ asset($student->image_path ?? 'img/male.png') }}" alt="" class="w-36 h-36 border-2  rounded-sm border-gray-900 p-1 mx-auto mt-10">
-        @endif
-
-    </div>
-    <div class="flex  mx-44">
-        <div class=" mt-20 text-md">نؤيد لكم ان <span class="font-semibold"> {{$student->name_ar}}</span>
-
-            <span class="text-lg">
-                @if($student->gender ==1)
-                الملصقة
-                @else
-                الملصق
-                @endif
-                @if($student->gender ==1)
-                صورتها
-                @else
-                صورته
-                @endif
-                اعلاه هو
-                @if($student->gender ==1)
-                احدى
-                @else
-                احد
-                @endif
-                @if($student->gender ==1)
-                خريجات
-                @else
-                خريجي
-                @endif
-                كليتنا للعام الدراسي
-                @if($student->unid->graduation_year == 1) 2014-2015 @elseif($student->unid->graduation_year == 2) 2015-2016 @elseif($student->unid->graduation_year == 3) 2016-2017 @elseif($student->unid->graduation_year == 4) 2017-2018 @elseif($student->unid->graduation_year == 5) 2018-2019 @elseif($student->unid->graduation_year == 6) 2019-2020 @elseif($student->unid->graduation_year == 7) 2020-2021 @elseif($student->unid->graduation_year == 8) 2021-2022 @elseif($student->unid->graduation_year == 9) 2022-2023 @elseif($student->unid->graduation_year == 10) 2023-2024 @elseif($student->unid->graduation_year == 11) 2024-2025 @elseif($student->unid->graduation_year == 12) 2025-2026 @elseif($student->unid->graduation_year == 13) 2026-2027 @elseif($student->unid->graduation_year == 14) 2027-2028 @elseif($student->unid->graduation_year == 15) 2028-2029 @elseif($student->unid->graduation_year == 16) 2029-2030 @endif
-                (
-                @if($student->unid->round ==0)
-                الدور الاول
-                @elseif($student->unid->round ==1)
-                الدور الثاني
-                @else
-                الدور الثالث
-                @endif
-                )
-                , قد
-                @if($student->gender ==1)
-                منحت
-                @else
-                منح
-                @endif
-
-                شهادة بكالوريوس العلوم في تكنولوجيا المعلومات/ علوم
-                الحاسوب/
-                @if($student->unid->type == 0)
-                الدراسة الصباحية
-                @else
-                الدراسة المسائية
-                @endif
-                في الفصل الدراسي للعام اعلاه ,ولا يعتبر هذا التأييد وثيقة تخرج
-                @if($selected)
-                لحين صدور الامر الجامعي الخاص بخريجي الدور على قسمه
-                @endif
-                وبناء على طلبه زود بهذا التأييد . </span>
+        <div class="flex justify-between mx-3">
+            <p>العدد:</p>
+            <p>:Ref</p>
+        </div>
+        <div class="flex justify-between mx-3">
+            <p>التاريخ:</p>
+            <p>:Date</p>
         </div>
     </div>
-    <div class="flex mx-auto mt-20"> مع التقدير</div>
-    <div class="grid grid-cols-2 mt-9 justify-between  text-center items-end">
-        <div class="">
-            <p class=" font-semibold mb-2 ">ثمينة محمد خلف</p>
-            <p> مسؤول وحدة الوثائق</p>
-        </div>
-        <div class="">
-            <p class="mb-2">الأستاذ الدكتور
-            <p class=" font-semibold mb-2"> زينب علي خلف</p>
-            <p class="">معاون العميد للشؤون العلمية والدراسات العليا</p>
-            </p>
-        </div>
 
+    {{-- * Body Compartment --}}
+    <div class="flex flex-col justify-between px-4 mx-auto ">
+        <div class="grid items-end grid-cols-3 ">
+            <div> </div>
+            <div class="items-center text-center">
+                <h2 class="text-xl font-semibold text-center">تأييد تخرج </h2>
+                <h2 class="text-xl font-semibold text-center">الى/من يمهمه الامر</h2>
+                <!-- @if ($selected)
+رقم التسلسل : <span class="font-semibold text-center "> {{ $student->id }}</span>
+@endif -->
+            </div>
+            @if ($student->gender == 1)
+                <img src="{{ asset($student->image_path ?? 'img/female.png') }}" alt=""
+                    class="p-1 mx-auto mt-10 border-2 border-gray-900 rounded-sm w-36 h-36">
+            @else
+                <img src="{{ asset($student->image_path ?? 'img/male.png') }}" alt=""
+                    class="p-1 mx-auto mt-10 border-2 border-gray-900 rounded-sm w-36 h-36">
+            @endif
+        </div>
+        <div class="flex mx-[100px]">
+            <div class="mt-20 text-md">نؤيد لكم ان <span class="font-semibold"> {{ $student->name_ar }}</span>
+
+                <span class="text-lg">
+                    @if ($student->gender == 1)
+                        الملصقة
+                    @else
+                        الملصق
+                    @endif
+                    @if ($student->gender == 1)
+                        صورتها
+                    @else
+                        صورته
+                    @endif
+                    اعلاه هو
+                    @if ($student->gender == 1)
+                        احدى
+                    @else
+                        احد
+                    @endif
+                    @if ($student->gender == 1)
+                        خريجات
+                    @else
+                        خريجي
+                    @endif
+                    كليتنا للعام الدراسي
+                    @if ($student->unid->graduation_year == 1)
+                        2014-2015
+                    @elseif($student->unid->graduation_year == 2)
+                        2015-2016
+                    @elseif($student->unid->graduation_year == 3)
+                        2016-2017
+                    @elseif($student->unid->graduation_year == 4)
+                        2017-2018
+                    @elseif($student->unid->graduation_year == 5)
+                        2018-2019
+                    @elseif($student->unid->graduation_year == 6)
+                        2019-2020
+                    @elseif($student->unid->graduation_year == 7)
+                        2020-2021
+                    @elseif($student->unid->graduation_year == 8)
+                        2021-2022
+                    @elseif($student->unid->graduation_year == 9)
+                        2022-2023
+                    @elseif($student->unid->graduation_year == 10)
+                        2023-2024
+                    @elseif($student->unid->graduation_year == 11)
+                        2024-2025
+                    @elseif($student->unid->graduation_year == 12)
+                        2025-2026
+                    @elseif($student->unid->graduation_year == 13)
+                        2026-2027
+                    @elseif($student->unid->graduation_year == 14)
+                        2027-2028
+                    @elseif($student->unid->graduation_year == 15)
+                        2028-2029
+                    @elseif($student->unid->graduation_year == 16)
+                        2029-2030
+                    @endif
+                    (
+                    @if ($student->unid->round == 0)
+                        الدور الاول
+                    @elseif($student->unid->round == 1)
+                        الدور الثاني
+                    @else
+                        الدور الثالث
+                    @endif
+                    )
+                    , قد
+                    @if ($student->gender == 1)
+                        منحت
+                    @else
+                        منح
+                    @endif
+
+                    شهادة بكالوريوس العلوم في تكنولوجيا المعلومات/ علوم
+                    الحاسوب/
+                    @if ($student->unid->type == 0)
+                        الدراسة الصباحية
+                    @else
+                        الدراسة المسائية
+                    @endif
+                    في الفصل الدراسي للعام اعلاه ,ولا يعتبر هذا التأييد وثيقة تخرج
+                    @if ($selected)
+                        لحين صدور الامر الجامعي الخاص بخريجي الدور على قسمه
+                    @endif
+                    وبناء على طلبه زود بهذا التأييد .
+                </span>
+            </div>
+        </div>
+        <div class="flex mx-auto mt-20 text-lg"> مع التقدير</div>
+
+        {{-- * Tail Compartment --}}
+        <div class="absolute bottom-[10%] grid items-end justify-between grid-cols-3 mt-24 text-center ">
+            <div class="">
+                <p class="mb-2 font-semibold ">ثمينة محمد خلف</p>
+                <p> مسؤول وحدة الوثائق</p>
+            </div>
+            <div class="">
+                <p class="mb-2">الأستاذ الدكتور
+                <p class="mb-2 font-semibold "> زينب علي خلف</p>
+                <p class="">معاون العميد للشؤون العلمية والدراسات العليا</p>
+                </p>
+            </div>
+
+        </div>
     </div>
+    <img class="absolute opacity-[30%] z-0 h-[575px] top-[25%] left-[13.8%]" src="/img/it.png" alt="water Mark">
+</page>
 
+<style scoped>
+    body {
+        background: #ffff;
+    }
 
-</div>
-</div>
+    page {
+        background: #edeac9;
+        display: block;
+        margin: 15px auto;
+        margin-bottom: 0.5cm;
+        box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+        border: #000;
+        border: dashed;
+    }
+
+    page[size="A4"] {
+        width: 21cm;
+        height: 29.7cm;
+    }
+
+    page[size="A4"][layout="landscape"] {
+        width: 29.7cm;
+        height: 21cm;
+    }
+
+    @media print {
+
+        body,
+        page {
+            margin: 0;
+            box-shadow: 0;
+        }
+    }
+</style>

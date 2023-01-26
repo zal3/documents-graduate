82b81d37568d36a51e694740f885c70709bed74c
zal3 <102377778+zal3@users.noreply.github.com>
2023-01-27 00:07:26 +0300

    a

--- app/Http/Livewire/Pages/Ui/Docpeople.php
+++ app/Http/Livewire/Pages/Ui/Docpeople.php
@@ -5,7 +5,7 @@ namespace App\Http\Livewire\Pages\Ui;
 use Livewire\Component;
 
 class Docpeople extends Component
-{
+{   public $selected2;
     public function render()
     {
         return view('livewire.pages.ui.docpeople');

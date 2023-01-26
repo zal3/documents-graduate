55f7558ed826e3061f84d65028fe4aa68c98188f
Hussein Najah <husseinnajah123@gmail.com>
2023-01-27 00:20:31 +0300

    Head Compartment

---
+++ app/Http/Livewire/Pages/Ui/Dochead.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Ui;
+
+use Livewire\Component;
+
+class Dochead extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.ui.dochead');
+    }
+}
